4afbc2e0876570f1201aa6f7af2c4d7e8722d4bc
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-06 11:38:13 -0500

    added topo to index

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var geojson2kml = require('geojson2kml'),
     geojson2shape = require('geojson2shape'),
-    geojson2csv = require('geojson2csv')//,
-    //topojson = require('topojson')
+    geojson2csv = require('geojson2csv'),
+    geojson2topojson = require('geojson2topojson')
 
 module.exports = {
   kml: geojson2kml,
   shp: geojson2shape,
-  csv: geojson2csv
+  csv: geojson2csv,
+  topojson: geojson2topojson
 }
